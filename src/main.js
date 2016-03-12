594024c011de33ad9d29799098e7c568661b334d
Simon Sturmer <sstur@me.com>
2016-03-12 15:46:47 +0800

    add getEntityRanges

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 export * from './Constants';
 export {default as Constants} from './Constants';
+
+export {default as getEntityRanges} from './getEntityRanges';
+
+export type {CharacterMetaList} from './getEntityRanges';
