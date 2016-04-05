848025cf5b4d8f59929f94f70eb22173f01bc5e7
Johannes Lumpe <johannes@johanneslumpe.de>
2016-04-05 15:46:41 +0300

    Added `selectionContainsEntity` util

--- src/main.js
+++ src/main.js
@@ -6,3 +6,4 @@ export {default as getEntityRanges} from './getEntityRanges';
 export type {CharacterMetaList} from './getEntityRanges';
 
 export {default as getSelectedBlocks} from './getSelectedBlocks';
+export {default as selectionContainsEntity} from './selectionContainsEntity';
