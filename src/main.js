d9126d98ecd4bced781cb20efaa03d60bce7bf0e
Johannes Lumpe <johannes@johanneslumpe.de>
2016-04-05 15:47:36 +0300

    Added export to main file

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ export {default as Constants} from './Constants';
 export {default as getEntityRanges} from './getEntityRanges';
 
 export type {CharacterMetaList} from './getEntityRanges';
+
+export { default as getSelectedBlocks } from './getSelectedBlocks';
