3db1d16315da5f896cd992d6fd1c318d438c9aa8
Johannes Lumpe <johannes@johanneslumpe.de>
2016-04-07 11:24:19 +0300

    Guard against invalid keys

--- src/getSelectedBlocks.js
+++ src/getSelectedBlocks.js
@@ -10,13 +10,24 @@
 export default (contentState, anchorKey, focusKey) => {
   const isSameBlock = anchorKey === focusKey;
   const startingBlock = contentState.getBlockForKey(anchorKey);
-  const selectedBlocks = [startingBlock];
+
+  if (!startingBlock) {
+    return [];
+  }
+
+  let selectedBlocks = [startingBlock];
 
   if (!isSameBlock) {
     let blockKey = anchorKey;
 
     while (blockKey !== focusKey) {
       const nextBlock = contentState.getBlockAfter(blockKey);
+
+      if (!nextBlock) {
+        selectedBlocks = [];
+        break;
+      }
+
       selectedBlocks.push(nextBlock);
       blockKey = nextBlock.getKey();
     }
